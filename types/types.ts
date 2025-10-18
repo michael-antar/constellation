b3e67cd65ce3dd7aaceba2f639e51e0eddce0d1c
michael-antar <michaeldantar@gmail.com>
2025-10-18 11:12:44 -0700

    Move create page api outside of auth folder

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,6 @@
+/* 
+Defines different categories a page can belong to. Used to classify pages for coloring nodes in the constellation and for filtering
+*/
 export type Category = {
   id: string; // Unique identifier for category: UUID PRIMARY KEY DEFAULT'gen_random_uuid()'
   name: string; // Display name for category: TEXT UNIQUE NOT NULL
