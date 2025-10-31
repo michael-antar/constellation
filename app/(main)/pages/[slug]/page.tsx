3c7da746ad3372e335cc57d4c04325ead87e6ff5
michael-antar <michaeldantar@gmail.com>
2025-10-31 13:45:19 -0700

    Update header colors

--- app/(main)/pages/[slug]/page.tsx
+++ app/(main)/pages/[slug]/page.tsx
@@ -5,20 +5,28 @@ import Link from "next/link";
 import React from "react";
 
 const components = {
+  // --- Headers ---
   h1: (props: React.ComponentProps<"h1">) => (
-    <h1 className="text-3xl font-bold text-white" {...props} />
+    <h1 className="text-5xl font-bold text-h1" {...props} />
   ),
   h2: (props: React.ComponentProps<"h2">) => (
-    <h2
-      className="text-2xl font-semibold text-purple-600 mt-8 mb-4"
-      {...props}
-    />
+    <h2 className="text-4xl font-semibold text-h2 mt-8 mb-4" {...props} />
   ),
   h3: (props: React.ComponentProps<"h3">) => (
-    <h3 className="text-xl font-semibold text-gray-800 mt-6 mb-2" {...props} />
+    <h3 className="text-3xl font-semibold text-h3 mt-6 mb-2" {...props} />
   ),
+  h4: (props: React.ComponentProps<"h4">) => (
+    <h3 className="text-2xl font-semibold text-h4 mt-6 mb-2" {...props} />
+  ),
+  h5: (props: React.ComponentProps<"h5">) => (
+    <h3 className="text-xl font-semibold text-h5 mt-6 mb-2" {...props} />
+  ),
+  h6: (props: React.ComponentProps<"h6">) => (
+    <h3 className="text-lg font-semibold text-h6 mt-6 mb-2" {...props} />
+  ),
+  // --- Basic Text ---
   p: (props: React.ComponentProps<"p">) => (
-    <p className="my-4 text-base leading-7" {...props} />
+    <p className="my-4 text-base text-p leading-7" {...props} />
   ),
   ul: (props: React.ComponentProps<"ul">) => (
     <ul className="list-disc list-inside my-4 pl-4" {...props} />
